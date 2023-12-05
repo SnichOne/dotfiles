2f3f549de44bfe3a099d099e32230b11df9b25a5
Sergey Nichiporchik <killserver@gmail.com>
2023-12-05 16:17:30 +0100

    Make NeoVim optional
    Install NeoVim plugins only if NeoVim is installed

--- run_once_after_install-nvim-plugins.sh
+++ run_once_after_install-nvim-plugins.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env sh
 
-# Create Neovim 'backupdir'.
-mkdir -p "${XDG_STATE_HOME:-$HOME/.local/state}"/nvim/backup/
-curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-nvim +PlugInstall +qall
-nvim +CocInstall +qall
+# Proceed only if nvim is installed
+if command -v nvim &> /dev/null
+then
+    # Create Neovim 'backupdir'.
+    mkdir -p "${XDG_STATE_HOME:-$HOME/.local/state}"/nvim/backup/
+    curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+    nvim +PlugInstall +qall
+    nvim +CocInstall +qall
+fi
